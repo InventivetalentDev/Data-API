a11b177bfdcc120c76f82248030d7d04829d8397
inventivetalent <inventivetalent@inventivegames.de>
2016-07-13 12:53:17 +0200

    add methods

--- src/main/java/org/inventivetalent/data/DataProvider.java
+++ src/main/java/org/inventivetalent/data/DataProvider.java
@@ -1,4 +1,32 @@
 package org.inventivetalent.data;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.Map;
+
 public interface DataProvider<V> {
+
+	void put(@Nonnull String key, @Nonnull String value);
+
+	void putAll(@Nonnull Map<String, V> map);
+
+	@Nullable
+	V get(@Nonnull String key);
+
+	boolean contains(@Nonnull String key);
+
+	void remove(@Nonnull String key);
+
+	@Nullable
+	V getAndRemove(@Nonnull String key);
+
+	@Nonnull
+	Collection<String> keys();
+
+	@Nonnull
+	Map<String, V> entries();
+
+	int size();
+
 }
